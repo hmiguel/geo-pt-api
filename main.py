9c4204d193321d5d1f577547187834314280ec3e
Hugo <hugo.s.conceicao@gmail.com>
2021-04-27 11:57:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 geo = geopt.GeoPT("Cont_AAD_CAOP2020.shp")
 
 @app.get("/")
-def get_location_info(lat: long, lon: long):
+def get_location_info(lat: float, lon: float):
     location = geo.get_location(lat, lon)
     return location if location else HTTPException(status_code=404, detail="Location not found: Coordinates out of range!")
