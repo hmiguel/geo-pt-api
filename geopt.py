da0c800dd65a673f53341ea1d5ba91c26385cd61
Hugo <hugo.s.conceicao@gmail.com>
2021-04-27 11:36:40 +0100

    Update geopt.py

--- geopt.py
+++ geopt.py
@@ -1,22 +1,23 @@
-import geopandas as gpd
+#import geopandas as gpd
 from shapely.geometry import shape, Point
 import pyproj
+import fiona
 
 class GeoPT(object):
         def __init__(self, shapefile):
-                self.data = gpd.read_file(shapefile)
+                self.data = fiona.open(shapefile)
                 self.transformer = pyproj.Transformer.from_crs("epsg:4326", self.data.crs.to_string())
 
-        def transform(self, lat, lon):
+        def __transform(self, lat, lon):
                 return self.transformer.transform(lat, lon)
 
         def get_location(self, lat, lon):
-                x, y = self.transform(lat, lon)
+                x, y = self.__transform(lat, lon)
                 point = Point(x,y)
-                for index, feature in self.data.iterrows():
-                        if shape(feature['geometry']).contains(point):
-                                return {"freguesia" : feature['Freguesia'], "concelho" : feature['Concelho'], "distrito" : feature["Distrito"] }
-                return None
+                #for index, feature in self.data.iterrows():
+                #        if shape(feature['geometry']).contains(point):
+                #                return {"freguesia" : feature['Freguesia'], "concelho" : feature['Concelho'], "distrito" : feature["Distrito"] }
+                return {'x':x, 'y':y}
 
 if __name__ == "__main__":
         geo = GeoPT("Cont_AAD_CAOP2020.shp")
