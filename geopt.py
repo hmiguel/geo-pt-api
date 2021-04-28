85db2a400d6041feb319781805615ab20e26b181
Hugo Conceicao <hugo@rijo.io>
2021-04-28 19:56:02 +0100

    add multi region search

--- geopt.py
+++ geopt.py
@@ -1,24 +1,36 @@
 from shapely.geometry import shape, Point
 import pyproj
-import fiona
+import geopandas as gpd
+import os
 
-class GeoPT(object):
+class Region(object):
         def __init__(self, shapefile):
-                self.data = fiona.open(shapefile)
-                self.transformer = pyproj.Transformer.from_crs("epsg:4326", self.data.crs.get('init', ''))
+                self.data = gpd.read_file(shapefile)
+                self.transformer = pyproj.Transformer.from_crs("epsg:4326", self.data.crs)
+                
+class GeoPT(object):
+        def __init__(self, shapedir):
+                self.regions = [Region(x) for x in self.__load_shapedir(shapedir)]
 
-        def __transform(self, lat, lon):
-                return self.transformer.transform(lat, lon)
+        def __load_shapedir(self, shape_path):
+                return [shape_path] if not os.path.isdir(shape_path) else [os.path.join(shape_path, file) for file in os.listdir(shape_path) if file.endswith(".shp")]
+
+        def __transform(self, region, lat, lon):
+                return region.transformer.transform(lat, lon)
 
         def get_location(self, lat, lon):
-                x, y = self.__transform(lat, lon)
-                point = Point(x,y)
-                for feature in self.data:
-                        if shape(feature['geometry']).contains(point):
-                                properties = feature.get('properties')
-                                return {"freguesia" : properties.get('Freguesia',None), "concelho" : properties.get('Concelho',None), "distrito" : properties.get('Distrito', None) }
+                for region in self.regions:
+                        x, y = self.__transform(region, lat, lon)
+                        point = Point(x,y)
+                        for index, feature in region.data.iterrows():
+                                if shape(feature['geometry']).contains(point):
+                                        return {"freguesia" : feature.get('Freguesia',None), "concelho" : feature.get('Concelho',None), "distrito" : feature.get('Distrito', None), "ilha": feature.get('Ilha', None) }                 
                 return None
 
 if __name__ == "__main__":
-        geo = GeoPT("Cont_AAD_CAOP2020.shp")
+        geo = GeoPT("shapes") 
         print(geo.get_location(40.153687,-8.514602))
+        print(geo.get_location(36.987866, -25.095279))
+        print(geo.get_location(37.797937, -25.348359))
+        print(geo.get_location(38.584861, -28.687783))
+        print(geo.get_location(32.765599, -17.053399))
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI, HTTPException
-import geopt
+import geopt, os
+
+abspath = os.path.dirname(os.path.abspath(__file__))
 
 app = FastAPI(title="Geo PT API",
     description="Detect official divisional administrative regions of Portugal",
     version="1.0.0",)
 
-geo = geopt.GeoPT("Cont_AAD_CAOP2020.shp")
+geo = geopt.GeoPT(os.path.join(abspath, "shapes"))
 
 @app.get("/")
 def get_location_info(lat: float, lon: float):
     location = geo.get_location(lat, lon)
     return location if location else HTTPException(status_code=404, detail="Location not found: Coordinates out of range!")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
